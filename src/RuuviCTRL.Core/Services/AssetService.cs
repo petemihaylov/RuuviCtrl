fc62fb1ca642a4f48b1a959aceda4ffa0f229706
Mickey krekels <42765197+MickeyKrekels@users.noreply.github.com>
2020-11-02 09:33:19 +0100

    Added a GetAssetDtoById fuction that get the ruuvi data based on start and end date.

--- src/RuuviCTRL.Core/Services/AssetService.cs
+++ src/RuuviCTRL.Core/Services/AssetService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using RuuviCTRL.Core.Dto;
@@ -22,12 +23,21 @@ namespace RuuviCTRL.Core.Services
 
         public async Task<AssetDto> GetAssetDtoById(int id)
         {
+            var assetDto = await GetAssetDtoById(id, DateTime.MinValue, DateTime.MaxValue);
+            return assetDto;
+        }
+
+        public async Task<AssetDto> GetAssetDtoById(int id, DateTime startDate, DateTime endDate)
+        {
             var asset = await _eFRepository.GetByIdAsync<Asset>(id);
             if (asset == null)
                 return null;
 
             var ruuviData = _repository.FilterBy(s => s.DeviceId == asset.DeviceId).ToList();
-            if (ruuviData.Count == 0)
+
+            var selectedRuuviData = ruuviData.Where(x => x.Time.Date >= startDate.Date && x.Time.Date <= endDate).ToList();
+
+            if (selectedRuuviData.Count == 0)
                 return null;
 
             var assetDto = new AssetDto
@@ -35,16 +45,15 @@ namespace RuuviCTRL.Core.Services
                 Id = asset.Id,
                 DeviceId = asset.DeviceId,
                 Name = asset.Name,
-                Temperature  = ruuviData.Select(c => new SingleStat { Value = c.Temperature, Time = c.Time }).ToArray(),
-                BatteryLevel = ruuviData.Select(c => new SingleStat { Value = c.BatteryLevel, Time = c.Time }).ToArray(),
-                Humidity = ruuviData.Select(c => new SingleStat { Value = c.Humidity, Time = c.Time }).ToArray(),
-                Pressure = ruuviData.Select(c => new SingleStat { Value = c.Pressure, Time = c.Time }).ToArray(),
-                Route = ruuviData.Select(c => new LocationStat { Latitude = c.Latitude, Longitude = c.Longitude, Time = c.Time }).ToArray()
+                Temperature = selectedRuuviData.Select(c => new SingleStat { Value = c.Temperature, Time = c.Time }).ToArray(),
+                BatteryLevel = selectedRuuviData.Select(c => new SingleStat { Value = c.BatteryLevel, Time = c.Time }).ToArray(),
+                Humidity = selectedRuuviData.Select(c => new SingleStat { Value = c.Humidity, Time = c.Time }).ToArray(),
+                Pressure = selectedRuuviData.Select(c => new SingleStat { Value = c.Pressure, Time = c.Time }).ToArray(),
+                Route = selectedRuuviData.Select(c => new LocationStat { Latitude = c.Latitude, Longitude = c.Longitude, Time = c.Time }).ToArray()
             };
 
             return assetDto;
         }
-
         public async Task<List<AssetDto>> GetAssetDtos()
         {
             var assets = await _eFRepository.ListAsync<Asset>();
