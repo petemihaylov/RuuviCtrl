f7cdd4918b9e93d763a432918919152db052c783
Matthijs Kotterink <poukimonster@gmail.com>
2020-11-10 15:19:57 +0100

    added breach fix

--- src/RuuviCTRL.WebSpa/ClientApp/src/app/pages/dashboard/dashboard-table/dashboard-table.component.ts
+++ src/RuuviCTRL.WebSpa/ClientApp/src/app/pages/dashboard/dashboard-table/dashboard-table.component.ts
@@ -5,6 +5,8 @@ import { AssetData } from '../_models/asset-data.model';
 import { AssetDto } from '../_models/assetDto.model';
 import { RuuviWebsocketService } from '../_services/ruuvi-websocket.service';
 import { BreachDto } from '../../../modules/ruuvi-monitoring/_models/breachDto.model';
+import { SlaDto } from '../../../modules/settings/_models/slaDto.model';
+import { AssetDetailService } from '../../../modules/ruuvi-monitoring/_services/asset-detail.service';
 
 @Component({
   selector: 'app-dashboard-table',
@@ -14,15 +16,51 @@ import { BreachDto } from '../../../modules/ruuvi-monitoring/_models/breachDto.m
 export class DashboardTableComponent implements OnInit {
 
   @Input('assetData') assetData: Observable<AssetDto[]>;
-  @Input('breachData') breachData: Observable<BreachDto[]>;
+  slaData: Observable<SlaDto[]>;
 
   //tempBreach;
   //pressureBreach;
   //humidityBreach;
 
-  constructor() { }
+    constructor(private assetdetailService: AssetDetailService) {
+        this.slaData = this.assetdetailService.getSlasForAsset(1);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        console.log(this.assetData);
+        console.log(this.slaData);
+    }
+
+    getTextColor(value: number, type: String) {
+        this.slaData.subscribe((sladata: SlaDto[]) => {
+            var data = sladata[sladata.length - 1];
+
+            switch (type) {
+                case 'Temperature':
+                    if (data.hasTempratureBoundry) {
+                        if (value > data.maxTemprature || value < data.minTemprature) {
+                            return 'red';
+                        }
+                    }
+                    break;
+                case 'Pressure':
+                    if (data.hasPressureBoundry) {
+                        if (value > data.maxPressure || value < data.minPressure) {
+                            return 'red';
+                        }
+                    }
+                    break;
+                case 'Humidity':
+                    if (data.hasHumidityBoundry) {
+                        if (value > data.maxHumidity || value < data.minHumidity) {
+                            return 'red';
+                        }
+                    }
+                    break;
+            }
+        });
+
+        return 'black';
+    }
 
 }
